cb82c5b9a3f40983237dce5cd5237e1574d4baab
diegoaferreira82 <diegoaferreira82@hotmail.com>
2026-02-03 16:50:18 -0400

    projeto inicial

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    //pegar a div e a tag "a"
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+}
