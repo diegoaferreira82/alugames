b436d8454d92287cfd61a068d6f768553f767872
diegoaferreira82 <diegoaferreira82@hotmail.com>
2026-02-03 17:03:20 -0400

    Final

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     //pegar a div e a tag "a"
-    let imagem = gameClicado.querySelector('.dashboard__item__img');
-    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let imagem = gameClicado.querySelector('.dashboard__item__img'); //div que tem a imagem
+    let botao = gameClicado.querySelector('.dashboard__item__button'); //botao
 
+    //consegue ver se está alugado com base na classe
+    if (imagem.classList.contains('dashboard__item__img--rented')) { //se está alugado
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.textContent = 'Alugar';
+        botao.classList.remove('dashboard__item__button--return');
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+    }
 }
